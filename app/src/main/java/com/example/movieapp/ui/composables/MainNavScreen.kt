bc85b76e02a06918d0618f4da3cb7ee7837e4ca2
Amit Kumar <ak363255@gmail.com>
2025-04-04 18:29:54 +0530

    Hilt Dep added

---
+++ app/src/main/java/com/example/movieapp/ui/composables/MainNavScreen.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.ui.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainNavScreen(modifier : Modifier = Modifier){
+}
--- app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.movieapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,39 +8,36 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MovieAppColor.Black,
+    onPrimary = MovieAppColor.White,
+    primaryContainer = MovieAppColor.RedC1,
+    background = MovieAppColor.Black,
+    surface = MovieAppColor.Black,
+    onSurface = MovieAppColor.White,
+    surfaceVariant = MovieAppColor.RedC1,
+    onSurfaceVariant = MovieAppColor.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = MovieAppColor.White,
+    onPrimary = MovieAppColor.Black,
+    primaryContainer = MovieAppColor.DarkGrayD3,
+    secondaryContainer = MovieAppColor.LightGrayD3,
+    surface = MovieAppColor.White,
+    surfaceVariant = MovieAppColor.White
 )
 
 @Composable
 fun MovieAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+          Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
@@ -52,7 +48,6 @@ fun MovieAppTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
