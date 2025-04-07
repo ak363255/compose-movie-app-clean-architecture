79c97768fa775a85166e171e6c605ec855e316e2
Amit <ak363255@gmail.com>
2025-04-07 21:12:38 +0530

    home page and search page added

--- data/src/main/java/com/example/data/urls/UrlConstants.kt
+++ data/src/main/java/com/example/data/urls/UrlConstants.kt
@@ -1,7 +1,7 @@
 package com.example.data.urls
 
 object UrlConstants {
-    val API_KEY = "43fccf02"
+    val API_KEY = ""
     const val BASE_URL = "https://www.omdbapi.com/"
     val MOVIE_URL = "${BASE_URL}?apikey=${API_KEY}&s=movie"
     val MOVIE_BY_ID_URL = "${BASE_URL}?apikey=${API_KEY}&plot=full"
